b0d9f2b918ecf3ae8fc98ced79422832af8a78df
Yusuf <yusufali.5094@gmail.com>
2025-08-15 22:28:31 +0530

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome to pizza house</h1>
+      <Button variant="outline">Button</Button>
     </main>
   );
 }
