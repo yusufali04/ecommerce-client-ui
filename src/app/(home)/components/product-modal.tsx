a29ab3fd45d0a4a7903432d5a4c4a0a0348e8121
Yusuf <yusufali.5094@gmail.com>
2025-08-19 20:55:26 +0530

    add default price config on page load

--- src/app/(home)/components/product-modal.tsx
+++ src/app/(home)/components/product-modal.tsx
@@ -21,7 +21,10 @@ type ChosenConfig = {
 
 const ProductModal = ({ product }: { product: Product }) => {
     const dispatch = useAppDispatch();
-    const [chosenConfig, setChosenConfig] = useState<ChosenConfig>();
+    const defaultConfiguration = Object.entries(product.category.priceConfiguration).map(([key, value]) => {
+        return { [key]: value.availableOptions[0] }
+    }).reduce((acc, curr) => ({ ...acc, ...curr }), {})
+    const [chosenConfig, setChosenConfig] = useState<ChosenConfig>(defaultConfiguration as unknown as ChosenConfig);
     const handleAddToCart = () => {
         const itemToAdd = {
             product: product,
