4dac1f7858678bebe07029e6132b5668d1a1b304
Yusuf <yusufali.5094@gmail.com>
2025-08-19 21:00:54 +0530

    fix cart items count

--- src/components/custom/cart-counter.tsx
+++ src/components/custom/cart-counter.tsx
@@ -1,16 +1,11 @@
 'use client'
-import { increment } from '@/lib/store/features/cart/cartSlice'
-import { useAppDispatch, useAppSelector } from '@/lib/store/hooks'
+import { useAppSelector } from '@/lib/store/hooks'
 import { ShoppingBasket } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 const CartCounter = () => {
-  const dispatch = useAppDispatch();
-  const value = useAppSelector((state) => state.cart.value)
-  const handleIncrement = () => {
-    dispatch(increment())
-  }
+  const cartItems = useAppSelector((state) => state.cart.cartItems)
 
   return (
     <>
@@ -19,10 +14,9 @@ const CartCounter = () => {
           <ShoppingBasket className='hover:text-primary' />
         </Link>
         <span className='absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white'>
-          {value}
+          {cartItems.length}
         </span>
       </div>
-      <button onClick={handleIncrement}>Increment</button>
     </>
 
   )
