e3ac54138727b4a1df0009a355ad1b2f4945fc19
Yusuf <yusufali.5094@gmail.com>
2025-08-27 22:10:03 +0530

    redirect to login page if not logged in

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,11 +1,16 @@
+import { getSession } from '@/lib/session';
 import CustomerForm from './components/customerForm';
+import { redirect } from 'next/navigation';
 
 export default async function Checkout({
     searchParams,
 }: {
     searchParams: { restaurant: string };
 }) {
-
+    const session = await getSession();
+    if (!session) {
+        redirect('/login')
+    }
     const sParams = new URLSearchParams(searchParams);
     const existingQueryString = sParams.toString();
 
