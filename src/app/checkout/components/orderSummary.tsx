e93a64a488ecc41f691a76fdf2ec8b51f8c6c1b1
Yusuf <yusufali.5094@gmail.com>
2025-08-28 16:18:23 +0530

    order summary calculation

--- src/app/checkout/components/orderSummary.tsx
+++ src/app/checkout/components/orderSummary.tsx
@@ -1,10 +1,36 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import React from 'react';
+import { useAppSelector } from '@/lib/store/hooks';
+import { getItemTotal } from '@/lib/utils';
+import React, { useMemo, useState } from 'react';
 
+// Store and fetch this from the restaurant database
+const TAXES_PERCENTAGE = 18;
+// Store and fetch this from the restaurant database
+const DELIVERY_CHARGES = 50;
 const OrderSummary = () => {
+    const cartItems = useAppSelector((state) => state.cart.cartItems);
+    const [discountPercentage, setDiscountPercentage] = useState(0);
 
+    const subTotal = useMemo(() => {
+        return cartItems.reduce((acc, curr) => {
+            return acc + curr.qty * getItemTotal(curr);
+        }, 0)
+    }, [cartItems]);
+
+    const discountAmount = useMemo(() => {
+        return (subTotal * discountPercentage) / 100;
+    }, [subTotal, discountPercentage]);
+
+    const taxesAmount = useMemo(() => {
+        const amountAfterDiscount = subTotal - discountAmount;
+        return (amountAfterDiscount * TAXES_PERCENTAGE) / 100;
+    }, [subTotal, discountAmount]);
+
+    const grandTotal = useMemo(() => {
+        return subTotal - discountAmount + taxesAmount + DELIVERY_CHARGES;
+    }, [subTotal, taxesAmount, discountAmount]);
     return (
         <Card className="w-2/5 border-none h-auto self-start">
             <CardHeader>
@@ -13,26 +39,26 @@ const OrderSummary = () => {
             <CardContent className="grid gap-4 pt-6">
                 <div className="flex items-center justify-between">
                     <span>Subtotal</span>
-                    <span className="font-bold">₹{500}</span>
+                    <span className="font-bold">₹{subTotal}</span>
                 </div>
                 <div className="flex items-center justify-between">
                     <span>Taxes</span>
-                    <span className="font-bold">₹{500}</span>
+                    <span className="font-bold">₹{taxesAmount}</span>
                 </div>
                 <div className="flex items-center justify-between">
                     <span>Delivery charges</span>
-                    <span className="font-bold">₹{500}</span>
+                    <span className="font-bold">₹{DELIVERY_CHARGES}</span>
                 </div>
                 <div className="flex items-center justify-between">
                     <span>Discount</span>
-                    <span className="font-bold">₹{500}</span>
+                    <span className="font-bold">₹{discountAmount}</span>
                 </div>
                 <hr />
                 <div className="flex items-center justify-between">
                     <span className="font-bold">Order total</span>
                     <span className="font-bold flex flex-col items-end">
                         <span className={'line-through text-gray-400'}>
-                            ₹{500}
+                            ₹{grandTotal}
                         </span>
                     </span>
                 </div>
