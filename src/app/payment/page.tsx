8d19e6341a070f98f14272c4ce1de285ef9a7340
Yusuf <yusufali.5094@gmail.com>
2025-09-03 22:30:39 +0530

    make success page dynamic

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -2,46 +2,75 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
-import { ArrowLeft, CheckCircle2, LayoutDashboard, Store } from 'lucide-react';
+import { ArrowLeft, CheckCircle2, CircleX, LayoutDashboard, Store } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
 
-const Payment = () => {
+const Payment = ({ searchParams }: { searchParams: { success: string, orderId: string } }) => {
+    const isOrderSuccess = searchParams.success === 'true';
     return (
         <div className='flex flex-col items-center gap-4 w-full mt-10'>
-            <CheckCircle2 size={80} className='text-green-500' />
-            <h1 className='text-xl font-bold mt-2 text-center'>Order placed successfully</h1>
-            <p className='text-base font-semibold -mt-2'>Thank you for your order!</p>
-            <Card className='mt-6'>
-                <CardHeader className='p-4'>
-                    <CardTitle className='flex items-center items-start text-lg justify-between gap-12'>
-                        <div className='flex items-center gap-3'>
-                            <Store size={35} className='text-primary' />
-                            <span>Your order information</span>
-                        </div>
-                        <Badge className='text-base px-4' variant={'secondary'}>Confirmed</Badge>
-                    </CardTitle>
-                </CardHeader>
-                <Separator />
-                <CardContent className='pt-6'>
-                    <div className='flex items-center gap-2'>
-                        <LayoutDashboard size={20} />
-                        <h2 className='text-base font-medium'>Order reference: </h2>
-                        <Link href={'/'} className='underline'>132121213213213321</Link>
-                    </div>
-                    <div className='flex items-center gap-2 mt-2'>
-                        <LayoutDashboard size={20} />
-                        <h2 className='text-base font-medium'>Payment Status: </h2>
-                        <span>Paid</span>
-                    </div>
-                </CardContent>
-            </Card>
-            <Button asChild className="mt-6">
-                <Link href={'/'}>
-                    <ArrowLeft size={20} className='text-white' />
-                    <span>Place another order</span>
-                </Link>
-            </Button>
+            {
+                isOrderSuccess ? (
+                    <>
+                        <CheckCircle2 size={80} className='text-green-500' />
+                        <h1 className='text-xl font-bold mt-2 text-center'>Order placed successfully</h1>
+                        <p className='text-base font-semibold -mt-2'>Thank you for your order!</p>
+                    </>
+                ) : (
+                    <>
+                        <CircleX size={80} className='text-red-500' />
+                        <h1 className='text-xl font-bold mt-2 text-center'>Payment failed</h1>
+                        <p className='text-base font-semibold -mt-2'>Failed to place your order. Please try again</p>
+                    </>
+                )
+            }
+            {
+                isOrderSuccess && (
+                    <Card className='mt-6'>
+                        <CardHeader className='p-4'>
+                            <CardTitle className='flex items-center items-start text-lg justify-between gap-12'>
+                                <div className='flex items-center gap-3'>
+                                    <Store size={35} className='text-primary' />
+                                    <span>Your order information</span>
+                                </div>
+                                <Badge className='text-base px-4' variant={'secondary'}>Confirmed</Badge>
+                            </CardTitle>
+                        </CardHeader>
+                        <Separator />
+                        <CardContent className='pt-6'>
+                            <div className='flex items-center gap-2'>
+                                <LayoutDashboard size={20} />
+                                <h2 className='text-base font-medium'>Order reference: </h2>
+                                <Link href={`/order-status/${searchParams.orderId}`} className='underline'>{searchParams.orderId}</Link>
+                            </div>
+                            <div className='flex items-center gap-2 mt-2'>
+                                <LayoutDashboard size={20} />
+                                <h2 className='text-base font-medium'>Payment Status: </h2>
+                                <span>Paid</span>
+                            </div>
+                        </CardContent>
+                    </Card>
+                )
+            }
+            {
+                isOrderSuccess ? (
+                    <Button asChild className="mt-6">
+                        <Link href={'/order/131654'}>
+                            <ArrowLeft size={20} className='text-white' />
+                            <span>Track order</span>
+                        </Link>
+                    </Button>
+                ) : (
+                    <Button asChild className="mt-6">
+                        <Link href={'/checkout'}>
+                            <ArrowLeft size={20} className='text-white' />
+                            <span>Try Again</span>
+                        </Link>
+                    </Button>
+                )
+            }
+
         </div>
     )
 }
