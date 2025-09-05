a8ca57c93ea09cfc3404fb1546407d19a77059b7
Yusuf <yusufali.5094@gmail.com>
2025-09-05 23:03:42 +0530

    show no orders message

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -27,7 +27,7 @@ const Orders = async () => {
     if (!response.ok) {
         throw new Error("Failed to fetch orders")
     }
-    const orders = await response.json()
+    const orders: Order[] = await response.json() || [];
 
     return (
         <Card className='max-w-screen-lg mx-auto mt-10'>
@@ -36,40 +36,49 @@ const Orders = async () => {
                 <p className='text-sm'>Your order history</p>
             </CardHeader>
             <CardContent>
-                <Table>
-                    <TableHeader className='bg-muted h-[50px]'>
-                        <TableRow>
-                            <TableHead className="w-[100px]">ID</TableHead>
-                            <TableHead className="text-left">Payment Status</TableHead>
-                            <TableHead className="text-left">Payment Mode</TableHead>
-                            <TableHead className="text-left">Date/Time</TableHead>
-                            <TableHead className="text-center">Order Status</TableHead>
-                            <TableHead className="text-center">Amount</TableHead>
-                            <TableHead className="text-right">Details</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {
-                            orders.map((order: Order) => {
-                                return (
-                                    <TableRow key={order._id} className='h-[70px]'>
-                                        <TableCell className="font-medium">{order._id}</TableCell>
-                                        <TableCell className="text-left">{(order.paymentStatus).charAt(0).toUpperCase() + (order.paymentStatus).slice(1).toLowerCase()}</TableCell>
-                                        <TableCell className="text-left">{(order.paymentMode).charAt(0).toUpperCase() + (order.paymentMode).slice(1).toLowerCase()}</TableCell>
-                                        <TableCell className="text-left">{new Date(order.createdAt).toLocaleString()}</TableCell>
-                                        <TableCell className="text-center">
-                                            <Badge variant={statusVariant[order.orderStatus] ?? "default"}>
-                                                {order.orderStatus.charAt(0).toUpperCase() + order.orderStatus.slice(1).toLowerCase()}
-                                            </Badge>
-                                        </TableCell>
-                                        <TableCell className="text-center">₹{order.total}</TableCell>
-                                        <TableCell className="text-right text-primary underline"><Link href={`/order/${order._id}`}>More details</Link></TableCell>
-                                    </TableRow>
-                                )
-                            })
-                        }
-                    </TableBody>
-                </Table>
+                {
+                    orders.length === 0 ? (
+                        <div className='text-center py-10'>
+                            <p className='text-sm'>You have no orders yet. <Link href="/" className='text-primary underline'>Go place an order</Link></p>
+                        </div>
+                    )
+                        :
+                        <Table>
+                            <TableHeader className='bg-muted h-[50px]'>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">ID</TableHead>
+                                    <TableHead className="text-left">Payment Status</TableHead>
+                                    <TableHead className="text-left">Payment Mode</TableHead>
+                                    <TableHead className="text-left">Date/Time</TableHead>
+                                    <TableHead className="text-center">Order Status</TableHead>
+                                    <TableHead className="text-center">Amount</TableHead>
+                                    <TableHead className="text-right">Details</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {
+                                    orders.map((order: Order) => {
+                                        return (
+                                            <TableRow key={order._id} className='h-[70px]'>
+                                                <TableCell className="font-medium">{order._id}</TableCell>
+                                                <TableCell className="text-left">{(order.paymentStatus).charAt(0).toUpperCase() + (order.paymentStatus).slice(1).toLowerCase()}</TableCell>
+                                                <TableCell className="text-left">{(order.paymentMode).charAt(0).toUpperCase() + (order.paymentMode).slice(1).toLowerCase()}</TableCell>
+                                                <TableCell className="text-left">{new Date(order.createdAt).toLocaleString()}</TableCell>
+                                                <TableCell className="text-center">
+                                                    <Badge variant={statusVariant[order.orderStatus] ?? "default"}>
+                                                        {order.orderStatus.charAt(0).toUpperCase() + order.orderStatus.slice(1).toLowerCase()}
+                                                    </Badge>
+                                                </TableCell>
+                                                <TableCell className="text-center">₹{order.total}</TableCell>
+                                                <TableCell className="text-right text-primary underline"><Link href={`/order/${order._id}`}>More details</Link></TableCell>
+                                            </TableRow>
+                                        )
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                }
+
             </CardContent>
         </Card>
     )
